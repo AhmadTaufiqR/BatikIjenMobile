20a17fef6732303740ce0d7445d1fca98023c8f9
AhmadTaufiqR <e41211969@student.ac.id>
2022-10-10 17:20:53 +0700

    memperbaiki desain dan bug

--- lib/view/all/login_page.dart
+++ lib/view/all/login_page.dart
@@ -1,95 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:ijen_batik/view/all/register_page.dart';
+import 'package:ijen_batik/view/components/background.dart';
 
-class LoginPage extends StatelessWidget {
+class Login_page extends StatelessWidget {
+  const Login_page({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Stack(
-          children: [
-            Image.asset(
-              "assets/img/back2.png",
-              fit: BoxFit.cover,
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-            ),
-            ListView(
-              children: [
-                SingleChildScrollView(
-                  padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.3,
-                      right: 15.0,
-                      left: 15.0),
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.only(right: 305.0, bottom: 10.0),
-                        child: Text(
-                          "Email",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.email),
-                          hintText: "Email",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
+    return Stack(
+      children: [
+        Background_image(assets: "assets/img/back2.png"),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Container(
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(
+                              right: 290.0,
+                              top: MediaQuery.of(context).size.height * 0.30),
+                          child: Text(
+                            "Username",
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(right: 280.0, bottom: 10.0),
-                        child: Text(
-                          "Password",
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                        SizedBox(
+                          height: 5,
                         ),
-                      ),
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.lock),
-                          hintText: "Password",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
+                        Container(
+                          padding: EdgeInsets.only(right: 15.0, left: 15.0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.account_circle),
+                              hintText: "Username",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: 25,
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(right: 290),
+                          child: Text(
+                            "Password",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(right: 15, left: 15),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.lock),
+                                hintText: "Password",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20))),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(right: 238),
+                          child: Text("Forgot Password?"),
+                        ),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: Size(200, 38),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                ),
+                                onPressed: () {},
+                                child: Text(
+                                  'LOGIN',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100,
+                        ),
+                        Container(
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => register_page()),
+                              );
+                            },
+                            child: Text("Belum Punya Akun?"),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size(200, 38),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          'LOGIN',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
--- lib/view/all/register_page.dart
+++ lib/view/all/register_page.dart
@@ -1,134 +1,169 @@
 import 'package:flutter/material.dart';
+import 'package:ijen_batik/view/all/login_page.dart';
+import 'package:ijen_batik/view/components/background.dart';
 
 class register_page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Stack(
-          children: [
-            Image.asset(
-              "assets/img/back3.png",
-              fit: BoxFit.cover,
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-            ),
-            ListView(
-              children: [
-                SingleChildScrollView(
-                  padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.25,
-                      right: 15.0,
-                      left: 15.0),
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.only(right: 290.0, bottom: 10.0),
-                        child: Text(
-                          "Username",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.account_circle),
-                          hintText: "Username",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
+    return Stack(
+      children: [
+        Background_image(assets: "assets/img/back3.png"),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Container(
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(
+                              right: 290.0,
+                              top: MediaQuery.of(context).size.height * 0.24),
+                          child: Text(
+                            "Username",
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(right: 315.0, bottom: 10.0),
-                        child: Text(
-                          "Email",
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                        SizedBox(
+                          height: 5,
                         ),
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.lock),
-                          hintText: "Email",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
+                        Container(
+                          padding: EdgeInsets.only(right: 15.0, left: 15.0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.account_circle),
+                              hintText: "Username",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(right: 290.0, bottom: 10.0),
-                        child: Text(
-                          "Password",
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                        SizedBox(
+                          height: 20,
                         ),
-                      ),
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.lock),
-                          hintText: "Password",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
+                        Container(
+                          padding: EdgeInsets.only(
+                            right: 317.0,
+                          ),
+                          child: Text(
+                            "Email",
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(right: 250.0, bottom: 10.0),
-                        child: Text(
-                          "Ulangi Password",
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                        SizedBox(
+                          height: 5,
                         ),
-                      ),
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.lock),
-                          hintText: "Ulangi Password",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
+                        Container(
+                          padding: EdgeInsets.only(right: 15.0, left: 15.0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.mail),
+                              hintText: "Email",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(right: 290),
+                          child: Text(
+                            "Password",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(right: 15, left: 15),
+                          child: TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.lock),
+                                hintText: "Password",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20))),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(right: 250),
+                          child: Text(
+                            "Ulangi Password",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(right: 15, left: 15),
+                          child: TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.lock),
+                                hintText: "Ulangi Password",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20))),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: Size(200, 38),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                ),
+                                onPressed: () {},
+                                child: Text(
+                                  'REGISTER',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Container(
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Login_page()),
+                              );
+                            },
+                            child: Text("Sudah Punya Akun?"),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size(200, 38),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          'REGISTER',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
