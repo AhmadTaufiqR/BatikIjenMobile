e24fe0d7516bf8472953bab667c72079eb488e32
AhmadTaufiqR <e41211969@student.ac.id>
2022-10-11 17:57:13 +0700

    menghapus file dash.dart

--- lib/view/all/dash.dart
+++ lib/view/all/dash.dart
@@ -10,34 +10,6 @@ class home_screen extends StatefulWidget {
 class _home_screenState extends State<home_screen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: SafeArea(
-        child: ListView(
-          padding: const EdgeInsets.only(left: 20, right: 20),
-          physics: const BouncingScrollPhysics(),
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [Text('Login'), Text('master')],
-            ),
-            Container(
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment(1, 0.0),
-                  colors: <Color>[
-                    Color(0xFF1A62AD),
-                    Color(0xFF00A2E9),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
