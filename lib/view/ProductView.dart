779bdcb5fa4b365715a085f28ee3d4b57385e17f
AhmadTaufiqR <e41211969@student.ac.id>
2022-11-17 19:12:34 +0700

    screen view mobile

---
+++ lib/view/ProductView.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ProductView extends StatefulWidget {
+  const ProductView({Key? key}) : super(key: key);
+
+  @override
+  State<ProductView> createState() => _ProductViewState();
+}
+
+class _ProductViewState extends State<ProductView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
